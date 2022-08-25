42a6a3687421ab5e9a72e2efeec2300d960a878a
Dmitrij Sanator <32552584+santr1901@users.noreply.github.com>
2022-08-25 12:14:40 +0300

    addSearch

--- src/Pages/Movies/Movies.jsx
+++ src/Pages/Movies/Movies.jsx
@@ -1,12 +1,50 @@
 // import { Outlet } from 'react-router-dom';
+import { useState } from 'react';
+import { searchMovie } from 'service';
+import { Link } from 'react-router-dom';
 
 const Movies = () => {
+  const [searchName, setSearchName] = useState('');
+  const [searchedMovies, setSearchedMovies] = useState([]);
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    searchMovie(searchName).then(response => {
+      const Results = response.results.map(movie => ({
+        id: movie.id,
+        name: movie.name || movie.title,
+      }));
+      setSearchedMovies([...Results]);
+    });
+  };
+
+  const handleChangeName = event => {
+    setSearchName(event.currentTarget.value.toLowerCase());
+  };
+
+  console.log(searchedMovies);
+
+  const addSearchedMovies = () => {
+    return searchedMovies.map(movie => (
+      <li key={movie.id}>
+        <Link exact="true" to={`/Movies/${movie.id}`}>
+          {movie.name}
+        </Link>
+      </li>
+    ));
+  };
+
   return (
     <div>
-      <form>
-        <input placeholder="input movie name"></input>
+      <form onSubmit={handleSubmit}>
+        <input
+          value={searchName}
+          onChange={handleChangeName}
+          placeholder="input movie name"
+        ></input>
         <button>Search</button>
       </form>
+      <ul>{addSearchedMovies()}</ul>
     </div>
   );
 };
--- src/service.js
+++ src/service.js
@@ -36,3 +36,11 @@ export async function getReviews(id) {
 
   return response.data;
 }
+
+export async function searchMovie(movieName) {
+  const response = await axios.get(
+    `https://api.themoviedb.org/3/search/movie/?api_key=${API_KEY}&language=en-US&query=${movieName}&page=1&include_adult=false`
+  );
+
+  return response.data;
+}
