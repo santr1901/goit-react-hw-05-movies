118f2a4533c31b9e1de777efe49b062cf8be5e30
Dmitrij Sanator <32552584+santr1901@users.noreply.github.com>
2022-08-29 11:50:43 +0300

    Update service.js

--- src/service.js
+++ src/service.js
@@ -37,7 +37,7 @@ export async function getReviews(id) {
 
 export async function searchMovie(movieName) {
   const response = await axios.get(
-    `https://api.themoviedb.org/3/search/movie/?api_key=${API_KEY}&language=en-US&query=${movieName}&page=1&include_adult=false`
+    `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=en-US&query=${movieName}&page=1&include_adult=false`
   );
 
   return response.data;
